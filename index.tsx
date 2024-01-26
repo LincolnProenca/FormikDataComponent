3552f19765594c71e4a6609043b9bc61db1f7a39
Lincoln Rodrigues Proença <91763571+LincolnProenca@users.noreply.github.com>
2024-01-25 22:31:55 -0300

    Update 1.0.1

---
+++ index.tsx
@@ -0,0 +1,7 @@
+import FormdataForm from "./src/components/FormdataForm";
+import FormDataRoot from "./src/components/FormdataRoot";
+
+export const FormikData = {
+  Root: FormDataRoot,
+  Form: FormdataForm,
+};
--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-import FormdataForm from "./FormdataForm";
-import FormDataRoot from "./FormdataRoot";
-
-export const FormikData = {
-  Root: FormDataRoot,
-  Form: FormdataForm,
-};
